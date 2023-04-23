976ba59800869758eb7b2eaf0837661d567252f1
kkintzell <kkintzel@villanova.edu>
2023-04-23 15:32:40 -0400

    Updated Foreign Keys and Attributes

--- fantasycollegebball.sql
+++ fantasycollegebball.sql
@@ -3,11 +3,12 @@
 --need to add foreign keys
 
 CREATE TABLE LEAGUE(
-    Name        VarChar(20)       NOT NULL,
+    LeagueName        VarChar(20)       NOT NULL,
     LeagueID    Int               NOT NULL,
     ManagerID   Int               NOT NULL,
     Max_No_Players  Int           NOT NULL,
-    CONSTRAINT League_PK PRIMARY KEY(LeagueID)
+    CONSTRAINT League_PK PRIMARY KEY(LeagueID),
+    CONSTRAINT MID_FK FOREIGN KEY (ManagerID) references User(UserID)
     );
 
 CREATE TABLE USER(
@@ -21,11 +22,16 @@ CREATE TABLE USER(
     );
 
 CREATE TABLE FANTASY_TEAM(
+    TeamID  Int NOT NULL,
+    UserID Int   NOT NULL,
     LeagueID  Int NOT NULL,
-    ManagerID Int   NOT NULL,
-    LeagueName  VarChar(20) NOT NULL,
-    Max_No_Players  Int NOT NULL,
-    CONSTRAINT LeagueID_PK PRIMARY KEY(LeagueID)
+    TeamName  VarChar(20) NOT NULL,
+    Wins Int,
+    Losses Int,
+    Draws Int,
+    CONSTRAINT TID_PK PRIMARY KEY(TeamID, UserID, LeagueID),
+    CONSTRAINT UID_FK FOREIGN KEY (UserID) references User(UserID),
+    CONSTRAINT LID_FK FOREIGN KEY (LeagueID) references League(LeagueID)
     );
 
 CREATE TABLE COLLEGE_PLAYERS(
@@ -42,10 +48,12 @@ CREATE TABLE Players_on_Team(
     TeamID      INT NOT NULL,
     UserID      INT NOT NULL,
     LeagueID    INT NOT NULL,
-    University  VarChar(20) NOT NULL,
-    CONSTRAINT PT_PK PRIMARY KEY(PlayerID, TeamID, UserID, LeagueID)
+    CONSTRAINT PT_PK PRIMARY KEY(PlayerID, TeamID, UserID, LeagueID),
+    CONSTRAINT PID_FK FOREIGN KEY (PlayerID) references COLLEGE_PLAYERS(PlayerID),
+    CONSTRAINT TID_FK FOREIGN KEY (TearmID) references FANTASY_TEAM(TeamID),
+    CONSTRAINT UID_FK FOREIGN KEY (UserID) references USER(UserID),
+    CONSTRAINT LID_FK FOREIGN KEY (LeagueID) references LEAGUE(LeagueID)
     );
-
 
 CREATE TABLE Match_Up(
     MatchID INT NOT NULL,
@@ -54,8 +62,10 @@ CREATE TABLE Match_Up(
     AwayID  INT NOT NULL,
     WeekNum INT NOT NULL,
     HomeWon VarChar(20) NOT NULL,
---What is homewon?
-    CONSTRAINT MU_PK PRIMARY KEY(MatchID, LeagueID, HomeID, AwayID)
+    CONSTRAINT MU_PK PRIMARY KEY(MatchID, LeagueID, HomeID, AwayID),
+    CONSTRAINT LID_FK FOREIGN KEY (LeagueID) references League(LeagueID),
+    CONSTRAINT HID_FK FOREIGN KEY (HomeID) references Team(TeamID),
+    CONSTRAINT AID_FK FOREIGN KEY (AwayID) references Team(TeadID)
     );
     
     
