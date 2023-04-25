3a5bc6ca375102cb799b779b4e0ba1c0f616de6a
Ryan Vaughan <ryvaughan17@gmail.com>
2023-04-24 20:05:37 -0400

    add player data

---
+++ scripts/data_to_sql.py
@@ -0,0 +1,22 @@
+import json
+
+data = json.load(open('playerdata.json', 'r'))
+
+def get_info(player):
+    s = player.split(' ')
+    first = s[0]
+    last = s[1][:-1]
+    
+    uni = player.split(',')[1].strip()
+    return (first, last, uni)
+
+def to_insert(id, first, last, uni):
+    return f'INSERT INTO COLLEGE_PLAYERS(PlayerID, FirstName, LastName, University) VALUES ({id}, {first}, {last}, {uni});'
+
+with open('playerInserts.sql', 'w') as file:
+    for i, v in enumerate(data):
+        first, last, uni = get_info(v['Player'])
+        file.write(to_insert(i, first, last, uni))
+        file.write('\n')
+
+
