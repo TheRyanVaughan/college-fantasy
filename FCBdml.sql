fe64be48c4f8b8e8f3aad8396a729301996ca721
Raf <95943156+rafaelbvelasquez@users.noreply.github.com>
2023-04-25 12:32:03 -0400

    Update FCBdml.sql

--- FCBdml.sql
+++ FCBdml.sql
@@ -1,6 +1,6 @@
 -- INPUT FOR THE LEAGUE TABLE
 INSERT INTO LEAGUE VALUES('Ballers', 100, 1, 6);
-INSERT INTO LEAGUE VALUES('Dunkers', 101, 6, 6);
+INSERT INTO LEAGUE VALUES('Dunkers', 101, 5, 6);
 INSERT INTO LEAGUE VALUES('Shootas', 102, 9, 6);
 
 
@@ -25,3 +25,24 @@ INSERT INTO APP_USERS VALUES(9,
     'Venkat', 'G', 'Margapuri;', 'vkat', to_date('1995-12-12', 'yyyy-mm-dd'));
 INSERT INTO APP_USERS VALUES(10, 
     'Kristin', 'B', 'Obermeyer;', 'kobermeyer', to_date('1990-09-10', 'yyyy-mm-dd'));
+INSERT INTO APP_USERS VALUES(11, 
+    'Daniel', 'J', 'Joyce;', 'djoyce', to_date('1985-03-15', 'yyyy-mm-dd'));
+INSERT INTO APP_USERS VALUES(10, 
+    'Frank', 'I', 'Klassner;', 'klassnerrrr', to_date('1985-06-03', 'yyyy-mm-dd'));
+    
+
+--INPUT FOR FANTASY_TEAM TABLE
+INSERT INTO FANTASY_TEAM VALUES(1, 1, 100, 'Team Raf', 2, 0, 0);
+INSERT INTO FANTASY_TEAM VALUES(2, 2, 100, 'Team Caleb', 2, 0, 0);
+INSERT INTO FANTASY_TEAM VALUES(3, 3, 100, 'Team Ryan', 1, 1, 0);
+INSERT INTO FANTASY_TEAM VALUES(4, 4, 100, 'Team Katie', 1, 1, 0);
+
+INSERT INTO FANTASY_TEAM VALUES(5, 5, 101, 'Team Jacob', 1, 1, 0);
+INSERT INTO FANTASY_TEAM VALUES(6, 6, 101, 'Team Jack', 1, 1, 0);
+INSERT INTO FANTASY_TEAM VALUES(7, 7, 101, 'Team Gabe', 1, 1, 0);
+INSERT INTO FANTASY_TEAM VALUES(8, 8, 101, 'Team Lauren', 1, 1, 0);
+
+INSERT INTO FANTASY_TEAM VALUES(9, 9, 102, 'Team Venkat', 2, 0, 0);
+INSERT INTO FANTASY_TEAM VALUES(10, 10, 102, 'Team Kristin', 2, 0, 0);
+INSERT INTO FANTASY_TEAM VALUES(11, 11, 102, 'Team Dan', 0, 2, 0);
+INSERT INTO FANTASY_TEAM VALUES(12, 12, 102, 'Team Klassner', 0, 2, 0);
