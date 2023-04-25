3267d5f6e7f35b49f529082847e90246a07160f2
Ryan Vaughan <ryvaughan17@gmail.com>
2023-04-24 20:13:35 -0400

    fix problematic chars

--- scripts/data_to_sql.py
+++ scripts/data_to_sql.py
@@ -3,15 +3,17 @@ import json
 data = json.load(open('playerdata.json', 'r'))
 
 def get_info(player):
+    player = player.replace("'", "")
+    player = player.replace("&", "")
     s = player.split(' ')
     first = s[0]
     last = s[1][:-1]
     
-    uni = player.split(',')[1].strip()
+    uni = player.split(',')[1].strip()[:20]
     return (first, last, uni)
 
 def to_insert(id, first, last, uni):
-    return f'INSERT INTO COLLEGE_PLAYERS(PlayerID, FirstName, LastName, University) VALUES ({id}, {first}, {last}, {uni});'
+    return f"INSERT INTO COLLEGE_PLAYERS(PlayerID, FirstName, LastName, University) VALUES ({id}, '{first}', '{last}', '{uni}');"
 
 with open('playerInserts.sql', 'w') as file:
     for i, v in enumerate(data):
